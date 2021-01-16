d5bfbc5cdc92b2d968e95f7d0d25b116c37a696c
Rick Home <rsladest@avans.nl>
2021-01-16 22:56:20 +0100

    Initial project setup

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "LeviathanDX"
+	configurations 
+	{
+		"Debug",
+		"Release"
+	}
+	architecture "x86_64"
+	
+	
+outputdir = "%{prj.name}/%{cfg.buildcfg}"
+IncludeDir = {}
+IncludeDir["Leviathan"] = "Leviathan/src"
+
+
+
+group "Dependencies"
+group ""
+
+project "LeviathanDX"
+	location "LeviathanDX"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "On"
+	systemversion "latest"
+	toolset "msc-v142"
+	
+	targetdir ("bin/" .. outputdir .. "/")
+	objdir ("bin-int/" .. outputdir .. "/")
+	
+	files 
+	{
+		"%{prj.name}/include/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+	
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{prj.name}/include"
+	}
+	
+	defines
+	{
+		"WIN32_LEAN_AND_MEAN"
+	}
+	
+	libdirs {"%{prj.name}/external/libs/%{cfg.architecture}"}
+	links 
+	{
+	}
+	
+	filter "configurations:Debug"
+		defines "LV_DEBUG"
+		symbols "On"
+		optimize "Off"
+	filter "configurations:Release"
+		defines {"LV_RELEASE", "NDEBUG"}
+		symbols "Off"
+		optimize "On"
+		
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "On"
+	
+	targetdir ("bin/" .. outputdir .. "/")
+	objdir ("bin-int/" .. outputdir .. "/")
+	
+	files 
+	{
+		"%{prj.name}/include/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+	
+	includedirs
+	{
+		"%{prj.name}/external/include",
+		"Leviathan/src",
+		"Leviathan/external/include",
+		"Leviathan/external/Glad/include",
+		"Leviathan/external/Glfw/include",
+		"C:/Program Files/boost/boost_1_74_0"
+	}
+	
+	defines
+	{
+		"WIN32_LEAN_AND_MEAN"
+	}
+	
+	links 
+	{
+		"LeviathanDX"
+	}
+	
+	filter "configurations:Debug"
+		defines "LV_DEBUG"
+		symbols "On"
+		optimize "Off"
+	filter "configurations:Release"
+		defines "LV_RELEASE"
+		symbols "Off"
+		optimize "On"
+	
